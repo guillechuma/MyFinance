188b58c549d25f790b1f77b580f0d571f9f8e54a
Guillermo Chumaceiro <guillechuma24@gmail.com>
2018-08-17 14:39:47 +0200

    Conforming to protocols in tableView

--- MyFinance/FinanceTableViewController.swift
+++ MyFinance/FinanceTableViewController.swift
@@ -29,24 +29,27 @@ class FinanceTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return expences.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = String(expences[indexPath.row])
 
         return cell
     }
-    */
-
+    
+    // This is a temporary data structure to test cells
+    let expences = [10, 20 ,30, 10, 45, 10.2]
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
